94645fdacaa1478d1a8d46cf45f89e66b4f39864
Kevin Gao <sudowork@gmail.com>
2014-11-17 19:14:32 -0800

    Initial commit

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+remote="https://github.com/sudowork/dotfiles.git"
+dir="dotfiles"
+full_path="$HOME/.$dir"
+
+if [ ! -d $full_path ]; then
+    echo "Installing YADR for the first time"
+    git clone $remote $full_path
+    cd $full_path
+    [ "$1" = "ask" ] && export ASK="true"
+    make install
+else
+    echo "dotfiles are already installed"
+fi
