e2e9069f49c95dc4a22ae1dd1638ff0c3c76367d
Kevin Gao <sudowork@gmail.com>
2020-09-11 11:48:58 -0700

    Don't activate ansible by default

--- oh-my-zsh/custom/sfdc.zsh
+++ oh-my-zsh/custom/sfdc.zsh
@@ -2,6 +2,6 @@ if [ -f /Users/kgao/.ansible/env.sh ]; then
   function activate_ansible() {
     . /Users/kgao/.ansible/env.sh
     # To disable ansible, comment out, but do not delete the following:
-    activate_ansible > /dev/null
+    # activate_ansible > /dev/null
   }
 fi
