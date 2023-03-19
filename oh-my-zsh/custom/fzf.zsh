2118eec502112eaf45a7915aff662cf2fbad6018
Kevin Gao <sudowork@gmail.com>
2023-03-18 17:31:24 -0700

    Fix fzf filepaths for arm64 macs

--- oh-my-zsh/custom/fzf.zsh
+++ oh-my-zsh/custom/fzf.zsh
@@ -1,14 +1,14 @@
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+  export PATH="${PATH:+${PATH}:}(brew --prefix)/opt/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "(brew --prefix)/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+source "$(brew --prefix)/opt/fzf/shell/key-bindings.zsh"
 
