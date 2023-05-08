f868a3ce413cfb9fbc04ed513b629e4fe794a914
Kevin Gao <kevin@appliedparameters.com>
2023-05-08 13:32:47 -0700

    Add id_ed25519 SSH key detection

--- oh-my-zsh/custom/ssh.zsh
+++ oh-my-zsh/custom/ssh.zsh
@@ -3,3 +3,9 @@ if [[ -f "$ssh_key" ]] &&
   ! ssh-add -l | grep -q "$(ssh-keygen -lf $ssh_key | awk '{print $2}')"; then
     ssh-add -K "$ssh_key"
 fi
+
+ssh_key="$HOME/.ssh/id_ed25519"
+if [[ -f "$ssh_key" ]] &&
+  ! ssh-add -l | grep -q "$(ssh-keygen -lf $ssh_key | awk '{print $2}')"; then
+    ssh-add --apple-use-keychain "$ssh_key"
+fi
